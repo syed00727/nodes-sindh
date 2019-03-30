06abcdb6f1d48820205f6910d9edc5c488b03028
mufaddal <mufaddal.tahir@careem.com>
2019-03-30 20:08:23 +0500

    Corrected cron job

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"log"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
-	"net/http"
-	"github.com/heroku/go-getting-started/controllers/web"
-	"github.com/heroku/go-getting-started/controllers/module"
-	"github.com/heroku/go-getting-started/controllers/webSocket"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/heroku/go-getting-started/controllers/module"
+	"github.com/heroku/go-getting-started/controllers/web"
+	"github.com/heroku/go-getting-started/controllers/webSocket"
+	_ "github.com/heroku/x/hmetrics/onload"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -31,12 +32,11 @@ func main() {
 	router.Use(gin.Logger())
 	router.Use(static.Serve("/", static.LocalFile(".", true)))
 
-
 	//APIs for module
 	router.POST("/node/status/:status", module.PostNodeStatus)
 
 	//web facing APIs
-	api:= router.Group("/api")
+	api := router.Group("/api")
 	{
 		api.GET("node/voltage/limit/:id", web.GetVoltageLimit)
 		api.POST("node/voltage/limit/:id", web.SetVoltageLimit)
@@ -50,7 +50,7 @@ func main() {
 	//web socket handshake
 	router.GET("ws", webSocket.WsHandshake)
 
-
+	log.Println(time.Now().Local().Truncate(60 * time.Minute))
 	router.GET("/health", func(c *gin.Context) {
 		c.String(http.StatusOK, "Thanks for asking about my health, Recovered from many serious blows today, How are you ?")
 	})
--- repos/nodePing.go
+++ repos/nodePing.go
@@ -2,13 +2,13 @@ package repos
 
 import (
 	"database/sql"
-	"os"
 	"fmt"
-	_ "github.com/lib/pq"
 	"github.com/heroku/go-getting-started/models/node"
-	"log"
-	"time"
+	_ "github.com/lib/pq"
 	"github.com/robfig/cron"
+	"log"
+	"os"
+	"time"
 )
 
 var db *sql.DB
@@ -43,7 +43,7 @@ func init() {
 }
 
 func scheduledFlush() {
-	db.Exec("delete from node_pings where ping_time::date < $1", time.Now().Local().Format("2018-10-23"))
+	db.Exec("delete from node_pings where ping_time::date < $1", time.Now().Local().Truncate(60*time.Minute))
 	log.Printf("Scheduled task was run @ %s", time.Now().Local())
 }
 
@@ -84,7 +84,37 @@ func GetLastPing(id int) (node.New, error) {
 }
 
 func UpdateNodeStatus(n node.New) error {
-	_, e := db.Exec("insert into node_pings(id, ping_time, status, battery_voltage,power_solar_input,power_battery_to_grid,power_grid_to_battery,power_battery_to_load,grid_voltage, switch_1, switch_2, load_switch_1, load_switch_2, load_switch_3, load_switch_4) values ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11, $12, $13, $14, $15)", n.Id, n.Ping, n.Status, n.BatteryVoltage, n.PowerSolarInput, n.PowerBatteryToGrid, n.PowerGridToBattery, n.PowerBatteryToLoad, n.GridVoltage, n.Switch1, n.Switch2, n.LoadSwitch1, n.LoadSwitch2, n.LoadSwitch3, n.LoadSwitch4)
+	_, e := db.Exec("insert into node_pings(id, "+
+		"ping_time, "+
+		"status, "+
+		"battery_voltage,"+
+		"power_solar_input,"+
+		"power_battery_to_grid,"+
+		"power_grid_to_battery,"+
+		"power_battery_to_load,"+
+		"grid_voltage, "+
+		"switch_1, "+
+		"switch_2, "+
+		"load_switch_1, "+
+		"load_switch_2, "+
+		"load_switch_3, "+
+		"load_switch_4) "+
+		"values ($1, $2, $3, $4, $5, $6, $7,$8,$9,$10,$11, $12, $13, $14, $15)",
+		n.Id,
+		n.Ping,
+		n.Status,
+		n.BatteryVoltage,
+		n.PowerSolarInput,
+		n.PowerBatteryToGrid,
+		n.PowerGridToBattery,
+		n.PowerBatteryToLoad,
+		n.GridVoltage,
+		n.Switch1,
+		n.Switch2,
+		n.LoadSwitch1,
+		n.LoadSwitch2,
+		n.LoadSwitch3,
+		n.LoadSwitch4)
 	return e
 }
 
@@ -106,18 +136,72 @@ func GetNodeIds() ([]int, error) {
 func GetLastPingForAllNodes() ([]node.New, error) {
 
 	pingList := make([]node.New, 0)
-	rows, e := db.Query("with ranked_messages as " +
-		"(select id,ping_time,status,battery_voltage,power_solar_input,power_battery_to_grid,power_grid_to_battery,power_battery_to_load,grid_voltage, switch_1, switch_2,load_switch_1,load_switch_2,load_switch_3,load_switch_4 ,rank() over (partition by id order by ping_time desc) as rn " +
-		"from node_pings) " +
-		"select ranked_messages.id,ping_time,status,battery_voltage,power_solar_input,power_battery_to_grid,power_grid_to_battery,power_battery_to_load,grid_voltage,switch_1,switch_2,load_switch_1,load_switch_2,load_switch_3,load_switch_4,limit_1,limit_2,limit_3,limit_4 from ranked_messages " +
-		"left join voltage_limit on voltage_limit.id = ranked_messages.id " +
-		"where rn = 1")
+	rows, e := db.Query(
+		"with ranked_messages as " +
+			"(" +
+			"select " +
+			"id," +
+			"ping_time," +
+			"status," +
+			"battery_voltage," +
+			"power_solar_input," +
+			"power_battery_to_grid," +
+			"power_grid_to_battery," +
+			"power_battery_to_load," +
+			"grid_voltage, " +
+			"switch_1, " +
+			"switch_2," +
+			"load_switch_1," +
+			"load_switch_2," +
+			"load_switch_3," +
+			"load_switch_4 ," +
+			"rank() over (partition by id order by ping_time desc) as rn " +
+			"from node_pings) " +
+			"select ranked_messages.id," +
+			"ping_time," +
+			"status," +
+			"battery_voltage," +
+			"power_solar_input," +
+			"power_battery_to_grid," +
+			"power_grid_to_battery," +
+			"power_battery_to_load," +
+			"grid_voltage," +
+			"switch_1," +
+			"switch_2," +
+			"load_switch_1," +
+			"load_switch_2," +
+			"load_switch_3," +
+			"load_switch_4," +
+			"limit_1," +
+			"limit_2," +
+			"limit_3," +
+			"limit_4 from ranked_messages " +
+			"left join voltage_limit on voltage_limit.id = ranked_messages.id " +
+			"where rn = 1")
 	if e != nil {
 		return pingList, e
 	}
 	var latestPing node.New
 	for rows.Next() {
-		if e := rows.Scan(&latestPing.Id, &latestPing.Ping, &latestPing.Status, &latestPing.BatteryVoltage, &latestPing.PowerSolarInput, &latestPing.PowerBatteryToGrid, &latestPing.PowerGridToBattery, &latestPing.PowerBatteryToLoad, &latestPing.GridVoltage, &latestPing.Switch1, &latestPing.Switch2, &latestPing.LoadSwitch1, &latestPing.LoadSwitch2, &latestPing.LoadSwitch3, &latestPing.LoadSwitch4, &latestPing.Limit1, &latestPing.Limit2, &latestPing.Limit3, &latestPing.Limit4); e == nil {
+		if e := rows.Scan(&latestPing.Id,
+			&latestPing.Ping,
+			&latestPing.Status,
+			&latestPing.BatteryVoltage,
+			&latestPing.PowerSolarInput,
+			&latestPing.PowerBatteryToGrid,
+			&latestPing.PowerGridToBattery,
+			&latestPing.PowerBatteryToLoad,
+			&latestPing.GridVoltage,
+			&latestPing.Switch1,
+			&latestPing.Switch2,
+			&latestPing.LoadSwitch1,
+			&latestPing.LoadSwitch2,
+			&latestPing.LoadSwitch3,
+			&latestPing.LoadSwitch4,
+			&latestPing.Limit1,
+			&latestPing.Limit2,
+			&latestPing.Limit3,
+			&latestPing.Limit4); e == nil {
 			pingList = append(pingList, latestPing)
 		} else {
 			break
